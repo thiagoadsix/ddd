cdfc5f278f86f3067def9c5df6a739c79874c108
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-02-19 22:58:33 -0300

    feat: implements the factory for product entity

---
+++ src/domain/product/factory/product.factory.spec.ts
@@ -0,0 +1,25 @@
+import { Product } from "../entity/product";
+import { ProductFactory } from "./product.factory";
+
+describe('Product Factory', () => {
+  it('should create a product type a', async () => {
+    const product = ProductFactory.create("a", "Product A", 1)
+
+    expect(product.id).toBeDefined()
+    expect(product.name).toBe("Product A")
+    expect(product.price).toBe(1)
+    expect(product.constructor.name).toBe("Product")
+  });
+  it('should create a product type b', async () => {
+    const product = ProductFactory.create("b", "Product B", 1)
+
+    expect(product.id).toBeDefined()
+    expect(product.name).toBe("Product B")
+    expect(product.price).toBe(2)
+    expect(product.constructor.name).toBe("ProductB")
+  });
+
+  it('should throw an error when product type is not supported', () => {
+    expect(() => ProductFactory.create('c', 'Product C', 1)).toThrowError('Product type not supported')
+  });
+});
